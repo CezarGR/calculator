6ba7c07b5d5b5918997dca9ca6ef297bf396d89b
CezarGR <51181637+CezarGR@users.noreply.github.com>
2022-08-31 20:26:28 -0300

    Create new project with VueJs 2

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+body {
+    background-image: url('./assets/background.svg');
+    height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@900&family=Roboto+Condensed:wght@700&family=Roboto:wght@400;500;700;900&display=swap');
+
+* {
+    margin: 0px;
+    font-family: 'Inter', sans-serif;
+    font-family: 'Roboto', sans-serif;
+    font-family: 'Roboto Condensed', sans-serif;
+    color: white;
+}
+
+</style>
